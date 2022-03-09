3b4bee5e876baa216347e8167672e041259ef199
Renato Costa <renatoaccosta@gmail.com>
2022-03-09 14:52:21 +0000

    lab done

--- app.js
+++ app.js
@@ -13,13 +13,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(__dirname + "/views/partials");
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+//since the API returns a promise we can use async!
+//different syntax than promise and await!!
+app.get('/beers', async(req, res) => {
+  const beers = await punkAPI.getBeers();
+  //console.log(beers);
+  res.render('beers', {beers});
+  
+});
+
+app.get('/random-beer', (req, res) => {
+  //const punkAPI2 = new PunkAPIWrapper();
+  const randomBeer = punkAPI.getRandom();
+  randomBeer
+  // .then is the response sending...therefore res.render INSIDE the then block
+    .then(beer => {
+      const randBeer = beer[0];
+      res.render('random-beer', {randBeer});
+    })
+    .catch(error => console.log(error));
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
