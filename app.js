3b863c23f09fd93ba1d0c4ac89a6ea24216a701b
Renato Costa <renatoaccosta@gmail.com>
2022-03-10 16:04:07 +0000

    Completed lab

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
@@ -21,16 +21,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-
 //since the API returns a promise we can use async!
 //different syntax than promise and await!!
-app.get('/beers', async(req, res) => {
+app.get('/beers', async (req, res) => {
   const beers = await punkAPI.getBeers();
-  //console.log(beers);
-  res.render('beers', {beers});
-  
+  res.render('beers', { beers });
 });
 
+app.get('/random-beer', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom();
+  const randBeer = randomBeer[0];
+  res.render('random-beer', { randBeer });
+});
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+// PROMISE VERSION
+/* 
 app.get('/random-beer', (req, res) => {
   //const punkAPI2 = new PunkAPIWrapper();
   const randomBeer = punkAPI.getRandom();
@@ -41,7 +48,4 @@ app.get('/random-beer', (req, res) => {
       res.render('random-beer', {randBeer});
     })
     .catch(error => console.log(error));
-});
-
-
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+}); */
